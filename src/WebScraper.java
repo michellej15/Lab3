63ff8b730da715296f7860684bced70b8674c5cb
michellejun <mj15@illinois.edu>
2018-09-19 14:12:08 -0500

    Commit 1: searching for word

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,49 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
     public static void main(String[] unused) {
-        System.out.println("Ran");
+        System.out.println("https://cs.illinois.edu/");
     }
+
+    public static int wordCount(String input) {
+        if (input == null || input.equals("")) {
+            return 0;
+        }
+        String[] words = input.split("");
+        return words.length;
+    }
+
+}
+    public static void nameSearch(String[] word, final String wordSearch) {
+        boolean search = false;
+        for (String word : word) {
+            if (word.equals(wordSearch)) {
+                search = true;
+                System.out.println(word.toLowerCase());
+            }
+        }
+    }
+
+
+
+/**
+ * Retrieve contents from a URL and return them as a string.
+ *
+ * @param url url to retrieve contents from
+ * @return the contents from the url as a string, or an empty string on error
+ */
+
+public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+        urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+        return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+        }
 }
